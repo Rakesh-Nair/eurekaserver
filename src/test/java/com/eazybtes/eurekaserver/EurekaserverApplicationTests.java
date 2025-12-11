d144145013e00a890d32e9f55284b90bfa9d41b9
Rakesh Nair <crucifyrakesh@gmail.com>
2025-12-11 21:54:10 +0530

    initial commit

---
+++ src/test/java/com/eazybtes/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybtes.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
